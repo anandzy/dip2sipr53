80a1cb733703014704b896f4d37fec6086d2a133
Anand <anandbhupathiraz@gmail.com>
2022-04-20 13:39:28 +0200

    Updates

--- chronetab.sh
+++ chronetab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Call this script in the crontab by logging to root user and execute 'crontab -e -u xinux'
+#Call this script in the crontab by logging to user and execute 'crontab -e -u $USER'
 #*/9 * * * * sh $HOME/dip2sipr53/chronetab.sh
 
 sed -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/"$(myip)"/ $HOME/dip2sipr53/aws_r53_alias_template.py > $HOME/dip2sipr53/aws_r53_alias.py && python3 $HOME/dip2sipr53/aws_r53_alias.py
